f5bc8fd04cd3cbd9282ca67e2c2f771e278cd580
msm9401 <anstmdals94@gmail.com>
2023-03-12 00:12:36 +0900

    emailfield 추가

--- users/models.py
+++ users/models.py
@@ -7,4 +7,5 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=150, blank=True, editable=False)
     last_name = models.CharField(max_length=150, blank=True, editable=False)
     name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, blank=True)
     profile_photo = models.URLField(blank=True)
